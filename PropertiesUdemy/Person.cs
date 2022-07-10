7bfd3416859fa00871dd1eeb62c35d8d35720672
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-10 13:14:21 +0200

    Add project files.

---
+++ PropertiesUdemy/Person.cs
@@ -0,0 +1,19 @@
+﻿//https://www.udemy.com/course/csharp-intermediate-classes-interfaces-and-oop/learn/lecture/2205242#content
+namespace PropertiesUdemy
+// right click on project -> open in terminal
+{
+    public class Person
+    {
+        public DateTime Birthdate { get; set; }
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+}
---
+++ PropertiesUdemy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.udemy.com/course/csharp-intermediate-classes-interfaces-and-oop/learn/lecture/2205242#content
+namespace PropertiesUdemy
+// right click on project -> open in terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Property is a class member that encapsulates a getter/setter
+            // for accessing a field.
+            // Property is used to create getter/setter wih less code.
+            var person = new Person();  
+            person.Birthdate = new DateTime(1982, 1, 1);
+        }
+    }
+}
